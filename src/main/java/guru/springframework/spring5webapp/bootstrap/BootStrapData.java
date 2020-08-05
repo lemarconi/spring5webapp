95588a4ac419e60dd884867a7651147f01d36165
lemarconi <leandro.marconi@gmail.com>
2020-08-05 14:01:57 -0300

    Primeiro commit

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,69 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        Publisher atica = new Publisher("Ed. Atica", "Rua Gomes, 222",
+                "Rio de Janeiro", "24000-001");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        publisherRepository.save(atica);
+
+        ddd.setPublisher(atica);
+        atica.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(atica);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        Publisher bBook = new Publisher("BrinkBook", "Juquis 204",
+                "São Paulo", "04081-010");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        publisherRepository.save(bBook);
+
+
+        noEJB.setPublisher(atica);
+        atica.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(atica);
+
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
+        System.out.println("Publisher number of books: " + bBook.getName() + " -> " + bBook.getBooks().size());
+        System.out.println("Publisher number of books: " + atica.getName() + " -> " + atica.getBooks().size());
+
+    }
+}
